d35dd5bd615a953f3cc4ef1cc0b937cd98879e2a
paulo <pmcpasd@gmail.com>
2023-07-11 18:47:29 -0400

    select multiple files

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,54 @@
 import * as vscode from 'vscode';
 import { JustFilesViewProvider } from './classes/justFilesViewProvider';
 import { FoldersViewProvider } from './classes/foldersViewProvider';
+import { FileItem } from './classes/fileItem';
 
 export function activate(context: vscode.ExtensionContext) {
+	let justFilesSelectedItems: readonly FileItem[] = [];
+	let filesSelectedItems: readonly FileItem[] = []
+
 	const justFilesViewProvider = new JustFilesViewProvider();
-	vscode.window.registerTreeDataProvider('justFilesView', justFilesViewProvider);
+	const justFilestreeView = vscode.window.createTreeView('justFilesView', { treeDataProvider: justFilesViewProvider, canSelectMany: true });
+	justFilestreeView.onDidChangeSelection((event) => {
+		justFilesSelectedItems = event.selection;
+	});
 
-	const showDisposable = vscode.commands.registerCommand('just-files.show', async (item) => {
-		await justFilesViewProvider.addFileItem(item);
+	const disposableShow = vscode.commands.registerCommand('just-files.show', async (fileItem) => {
+		const isFileItemInfilesSelectedItems = filesSelectedItems.some(item => item.resourceUri?.path === fileItem.resourceUri.path);
+		if (filesSelectedItems.length > 0 && isFileItemInfilesSelectedItems) {
+			filesSelectedItems.map(async item => {
+				await justFilesViewProvider.addFileItem(item);
+				justFilesViewProvider.refresh();
+			})
+			return;
+		}
+
+		await justFilesViewProvider.addFileItem(fileItem);
+		justFilesViewProvider.refresh();
+		
+	});
+
+	const disposableHide = vscode.commands.registerCommand('just-files.hide', (fileItem) => {
+		const isFileItemInjustFilesSelectedItems = justFilesSelectedItems.some(item => item.resourceUri?.path === fileItem.resourceUri.path);
+		if (justFilesSelectedItems.length > 0 && isFileItemInjustFilesSelectedItems) {
+			justFilesSelectedItems.map(item => {
+				justFilesViewProvider.addHideFileItem(item);
+				justFilesViewProvider.refresh();
+			})
+			return;
+		}
+		justFilesViewProvider.addHideFileItem(fileItem);
 		justFilesViewProvider.refresh();
 	});
 
-	const hideDisposable = vscode.commands.registerCommand('just-files.hide', (item) => {
-		justFilesViewProvider.addHideFileItem(item);
-		justFilesViewProvider.refresh();
-	});
-
-	context.subscriptions.push(showDisposable);
-	context.subscriptions.push(hideDisposable)
+	context.subscriptions.push(disposableShow);
+	context.subscriptions.push(disposableHide)
 
 	const foldersViewProvider = new FoldersViewProvider();
-	vscode.window.registerTreeDataProvider('filesView', foldersViewProvider);
-	vscode.commands.executeCommand('setContext', 'git.decorations.enabled', false);
+	const filesTreeView = vscode.window.createTreeView('filesView', { treeDataProvider: foldersViewProvider, canSelectMany: true });
+	filesTreeView.onDidChangeSelection((event) => {
+		filesSelectedItems = event.selection;
+	});
 
 	vscode.workspace.onDidChangeWorkspaceFolders(() => {
 		foldersViewProvider.refresh();
